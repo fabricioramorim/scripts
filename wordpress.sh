382c0857fe75d5edc900b7918b5feadf45d7b1db
fabricioramorim <38993485+fabricioramorim@users.noreply.github.com>
2022-10-03 17:32:03 -0300

    Create wordpress.sh

---
+++ wordpress.sh
@@ -0,0 +1,19 @@
+  #!/bin/bash
+
+  echo Script para instalacao base - Wordpress  
+  
+  echo Trocando de usuario...
+  
+  sudo su
+  
+  sleep 2s
+  
+  echo Atualizando o sistema...
+  
+  sleep 1s
+  
+  apt update && apt upgrade -y
+  
+  sleep 2s
+  
+ 
